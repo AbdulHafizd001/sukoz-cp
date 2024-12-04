9a90c4cc563bf6786f182654e51353ff3cd3bdd1
Abdul Hafizd Alghifari <137294390+AbdulHafizd001@users.noreply.github.com>
2024-12-04 07:12:48 +0700

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,8 @@ from flask import Flask
 from app.routes.user import bp as user_bp  # Import blueprint 'bp' dari 'user.py'
 from app.routes.recipe import recipe_bp 
 from app.routes.category import category_bp
-from app.routes.rating import rating_bp
 from app.routes.bookmark import bookmark_bp
-from app import models #bara aja di tambahin 
+from app import models
 
 def create_app():
     app = Flask(__name__)
@@ -12,6 +11,5 @@ def create_app():
     app.register_blueprint(user_bp, url_prefix='/api/v1')  # Register blueprint dengan url_prefix
     app.register_blueprint(recipe_bp, url_prefix='/api/v1')
     app.register_blueprint(category_bp, url_prefix='/api/v1')
-    app.register_blueprint(rating_bp, url_prefix='/api/v1')
     app.register_blueprint(bookmark_bp, url_prefix='/api/v1')
     return app
