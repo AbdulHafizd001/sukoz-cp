f79f90bc2ad57d1d6a366587ad70edddd9452c4b
Abdul Hafizd Alghifari <137294390+AbdulHafizd001@users.noreply.github.com>
2024-12-04 11:40:04 +0700

    Update __init__.py
    Menambahkan blueprint login

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
-from app.routes.user import bp as user_bp  # Import blueprint 'bp' dari 'user.py'
+from app.routes.user import user_bp  # Import blueprint 'bp' dari 'user.py'
+from app.routes.login import login_bp
 from app.routes.recipe import recipe_bp 
 from app.routes.category import category_bp
 from app.routes.bookmark import bookmark_bp
@@ -9,6 +10,7 @@ def create_app():
     app = Flask(__name__)
 
     app.register_blueprint(user_bp, url_prefix='/api/v1')  # Register blueprint dengan url_prefix
+    app.register_blueprint(login_bp, url_prefix='/api/v1') # Menambahkan blueprint login
     app.register_blueprint(recipe_bp, url_prefix='/api/v1')
     app.register_blueprint(category_bp, url_prefix='/api/v1')
     app.register_blueprint(bookmark_bp, url_prefix='/api/v1')
