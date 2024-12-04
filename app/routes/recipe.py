ecd250fb012117aeed02286dd0a901f2228cb2a1
Abdul Hafizd Alghifari <137294390+AbdulHafizd001@users.noreply.github.com>
2024-12-04 07:15:07 +0700

    Update recipe.py (recipes -> recipe)

--- app/routes/recipe.py
+++ app/routes/recipe.py
@@ -5,7 +5,7 @@ from app.models import add_recipe, get_recipe, update_recipe, delete_recipe
 recipe_bp = Blueprint('recipe', __name__)
 
 # Rute untuk membuat resep baru
-@recipe_bp.route('/recipes', methods=['POST'])
+@recipe_bp.route('/recipe', methods=['POST'])
 def create_recipe():
     data = request.get_json()
     name = data.get('name')
@@ -22,21 +22,20 @@ def create_recipe():
 
 
 # Rute untuk mendapatkan resep berdasarkan ID
-@recipe_bp.route('/recipes/<recipe_id>', methods=['GET'])
+@recipe_bp.route('/recipe/<recipe_id>', methods=['GET'])
 def get_recipe_by_id(recipe_id):
     recipe = get_recipe(recipe_id)
     if not recipe:
         return jsonify({"error": "Recipe not found"}), 404
     return jsonify({"recipe": recipe}), 200
 
-@recipe_bp.route('/recipes/<recipe_id>', methods=['PUT'])
+@recipe_bp.route('/recipe/<recipe_id>', methods=['PUT'])
 def modify_recipe(recipe_id):
     data = request.json
     update_recipe(recipe_id, data)
     return jsonify({"message": "Recipe updated successfully!"}), 200
 
-@recipe_bp.route('/recipes/<recipe_id>', methods=['DELETE'])
+@recipe_bp.route('/recipe/<recipe_id>', methods=['DELETE'])
 def remove_recipe(recipe_id):
     delete_recipe(recipe_id)
     return jsonify({"message": "Recipe deleted successfully!"}), 200
-
